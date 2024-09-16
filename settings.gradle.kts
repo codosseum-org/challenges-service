94d6a04b00d86bcbb512a0b4e2c9901eb2c8d7a2
Paul Kindler <pa179kin@gmail.com>
2024-09-16 22:23:41 +0200

    moved some dependencies

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,11 +6,10 @@ dependencyResolutionManagement {
 			version("kotlin", "2.0.0")
 			version("ktor", "3.0.0-beta-2")
 
-
-			plugin("jvm", "org.jetbrains.kotlin.jvm").versionRef("kotlin")
-			plugin("serialization", "org.jetbrains.kotlin.plugin.serialization").versionRef("kotlin")
 			plugin("ktor", "io.ktor.plugin").versionRef("ktor")
+			plugin("jvm", "org.jetbrains.kotlin.jvm").versionRef("kotlin")
 			plugin("openapi", "org.openapi.generator").version("7.8.0")
+			plugin("serialization", "org.jetbrains.kotlin.plugin.serialization").versionRef("kotlin")
 			plugin("ktorOpenapi", "io.github.tabilzad.ktor-docs-plugin-gradle").version("0.6.2-alpha")
 
 			version("coroutines", "1.9.0")
@@ -23,6 +22,7 @@ dependencyResolutionManagement {
 			).versionRef("serialization")
 
 			library("kotlinx.coroutines", "org.jetbrains.kotlinx", "kotlinx-coroutines-core").versionRef("coroutines")
+			library("kotlinx.hocon", "org.jetbrains.kotlinx", "kotlinx-serialization-hocon").versionRef("serialization")
 
 			bundle(
 				"kotlinx",
@@ -32,48 +32,49 @@ dependencyResolutionManagement {
 				)
 			)
 
-			library("ktor.server.cio", "io.ktor", "ktor-server-cio").withoutVersion()
-			library("ktor.server.netty", "io.ktor", "ktor-server-netty").withoutVersion()
-			library("ktor.server.core", "io.ktor", "ktor-server-core").withoutVersion()
-			library("ktor.server.resources", "io.ktor", "ktor-server-resources").withoutVersion()
-			library("ktor.server.negotiation", "io.ktor", "ktor-server-content-negotiation").withoutVersion()
-			library("ktor.server.logging", "io.ktor", "ktor-server-call-logging").withoutVersion()
-			library("ktor.serialization-json", "io.ktor", "ktor-serialization-kotlinx-json").withoutVersion()
-			library("ktor.client", "io.ktor", "ktor-client-core").withoutVersion()
-			library("ktor.client-serialization", "io.ktor", "ktor-client-serialization").withoutVersion()
-			library("ktor.clientnegotiation", "io.ktor", "ktor-client-content-negotiation").withoutVersion()
-			library("ktor.javahttp", "io.ktor", "ktor-client-java").withoutVersion()
-			library("ktor.micrometer", "io.ktor", "ktor-server-metrics-micrometer").withoutVersion()
 			library("ktor.sse", "io.ktor", "ktor-server-sse").withoutVersion()
+			library("ktor.client", "io.ktor", "ktor-client-core").withoutVersion()
+			library("ktor.javahttp", "io.ktor", "ktor-client-java").withoutVersion()
+			library("ktor.server.cio", "io.ktor", "ktor-server-cio").withoutVersion()
 			library("ktor.openapi", "io.ktor", "ktor-server-openapi").withoutVersion()
 			library("ktor.swagger", "io.ktor", "ktor-server-swagger").withoutVersion()
+			library("ktor.server.core", "io.ktor", "ktor-server-core").withoutVersion()
+			library("ktor.server.netty", "io.ktor", "ktor-server-netty").withoutVersion()
 			library("ktor.resources", "io.ktor", "ktor-server-resources").withoutVersion()
+			library("ktor.server.resources", "io.ktor", "ktor-server-resources").withoutVersion()
+			library("ktor.server.logging", "io.ktor", "ktor-server-call-logging").withoutVersion()
+			library("ktor.micrometer", "io.ktor", "ktor-server-metrics-micrometer").withoutVersion()
+			library("ktor.client.serialization", "io.ktor", "ktor-client-serialization").withoutVersion()
+			library("ktor.clientnegotiation", "io.ktor", "ktor-client-content-negotiation").withoutVersion()
+			library("ktor.serialization.json", "io.ktor", "ktor-serialization-kotlinx-json").withoutVersion()
+			library("ktor.server.negotiation", "io.ktor", "ktor-server-content-negotiation").withoutVersion()
 
 			bundle(
 				"ktor",
 				listOf(
+					"ktor.sse",
+					"ktor.client",
+					"ktor.openapi",
+					"ktor.swagger",
+					"ktor.javahttp",
+					"ktor.resources",
+					"ktor.micrometer",
 					"ktor.server.core",
 					"ktor.server.cio",
 					"ktor.server.netty",
-					"ktor.server.resources",
-					"ktor.server.negotiation",
 					"ktor.server.logging",
-					"ktor.serialization-json",
-					"ktor.client",
-					"ktor.client-serialization",
+					"ktor.server.resources",
 					"ktor.clientnegotiation",
-					"ktor.javahttp",
-					"ktor.micrometer",
-					"ktor.sse",
-					"ktor.openapi",
-					"ktor.swagger",
-					"ktor.resources"
+					"ktor.serialization-json",
+					"ktor.server.negotiation",
+					"ktor.client-serialization",
 				)
 			)
 
 			version("koin", "4.0.0-RC2")
 			library("koin", "io.insert-koin", "koin-core").versionRef("koin")
 			library("koin.ktor", "io.insert-koin", "koin-ktor").versionRef("koin")
+
 			bundle("koin", listOf("koin", "koin.ktor"))
 
 			library("kfswatch", "io.github.irgaly.kfswatch", "kfswatch").version("1.3.0")
@@ -93,8 +94,8 @@ dependencyResolutionManagement {
 			library("kaml", "com.charleskorn.kaml", "kaml").version("0.61.0")
 
 			version("jackson", "2.17.2")
-			library("jackson-db", "com.fasterxml.jackson.core", "jackson-databind").versionRef("jackson")
 			library("jackson-core", "com.fasterxml.jackson.core", "jackson-core").versionRef("jackson")
+			library("jackson-db", "com.fasterxml.jackson.core", "jackson-databind").versionRef("jackson")
 			library("jackson-annotations", "com.fasterxml.jackson.core", "jackson-annotations").versionRef("jackson")
 			library("jackson-yaml", "com.fasterxml.jackson.dataformat", "jackson-dataformat-yaml").versionRef("jackson")
 
