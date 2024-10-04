995820da14b46adac37ab6c9f7e9d8f711b3e52f
Paul Kindler <pa179kin@gmail.com>
2024-10-04 17:38:56 +0200

    make valide an extension function make use context of the service

--- src/main/kotlin/org/developerden/codosseum/validation/SolutionValidation.kt
+++ src/main/kotlin/org/developerden/codosseum/validation/SolutionValidation.kt
@@ -1,6 +1,5 @@
 package org.developerden.codosseum.validation
 
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.asFlow
 import kotlinx.coroutines.flow.channelFlow
@@ -8,6 +7,7 @@ import kotlinx.coroutines.flow.toSet
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import kotlinx.serialization.Serializable
+import org.developerden.codosseum.ChallengesService
 import org.developerden.codosseum.challenge.Challenge
 import org.developerden.codosseum.sandkasten.api.apis.ProgramsApi
 import org.developerden.codosseum.sandkasten.api.models.BuildRequest
@@ -16,39 +16,38 @@ import org.developerden.codosseum.sandkasten.api.models.RunRequest
 import org.developerden.codosseum.server.routes.event.EventBus
 import org.developerden.codosseum.server.routes.event.TestCompleteEvent
 
-suspend fun validateSolutions(
+suspend fun Challenge.validateSolutions(
   eventsBus: EventBus,
   api: ProgramsApi,
-  challenge: Challenge,
 ): SolutionValidationResult {
   val compiledSolution = api.compile(
     BuildRequest(
-      challenge.info.solution.language,
-      BuildRequestMainFile(challenge.solutionInputStream.readBytes().decodeToString())
+      info.solution.language,
+      BuildRequestMainFile(solutionInputStream.readBytes().decodeToString())
     )
   ).body()
 
 
-  val results =
-    challenge.info.publicTests.asFlow().concurrentMap { test ->
-      val body = api.runProgram(
-        compiledSolution.programId, RunRequest(
-          test.input.joinToString("\n")
-        )
-      ).body()
-      val succeeded = body.stdout.trim() == test.output.joinToString("\n")
-      eventsBus.publish(
-        TestCompleteEvent(
-          challenge.name, test.name, succeeded, if (succeeded) null else FailedTest(
-            test.name,
-            test.output.joinToString("\n"),
-            body.stdout.trim(),
-            body.stderr
-          )
+  val results = info.publicTests.asFlow().concurrentMap { test ->
+    val body = api.runProgram(
+      compiledSolution.programId, RunRequest(
+        test.input.joinToString("\n")
+      )
+    ).body()
+    val succeeded = body.stdout.trim() == test.output.joinToString("\n")
+    eventsBus.publish(
+      TestCompleteEvent(
+        name, test.name, succeeded,
+        if (succeeded) null else FailedTest(
+          test.name,
+          test.output.joinToString("\n"),
+          body.stdout.trim(),
+          body.stderr
         )
       )
-      test to body
-    }.toSet().toMap()
+    )
+    test to body
+  }.toSet().toMap()
 
 
   val failed = results
@@ -57,7 +56,7 @@ suspend fun validateSolutions(
       FailedTest(k.name, k.output.joinToString("\n"), v.stdout.trim(), v.stderr)
     }.toSet()
   val success = failed.isEmpty()
-  return SolutionValidationResult(challenge.info.title, success, failed)
+  return SolutionValidationResult(info.title, success, failed)
 }
 
 @Serializable
@@ -83,7 +82,7 @@ data class TemplateValidationResult(
 
 
 inline fun <T, R> Flow<T>.concurrentMap(crossinline transform: suspend (T) -> R): Flow<R> = channelFlow {
-  withContext(Dispatchers.IO) {
+  withContext(ChallengesService.coroutineContext) {
     collect {
       launch { send(transform(it)) }
     }
