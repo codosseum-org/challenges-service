d8cf93dde1982b108ca3038a4a64f8d63d205d30
Paul Kindler <pa179kin@gmail.com>
2025-02-24 18:19:38 +0100

    add indexing bases for remote source

---
+++ src/main/kotlin/org/developerden/codosseum/indexing/git/Repository.kt
@@ -0,0 +1,12 @@
+package org.developerden.codosseum.indexing.git
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class Repository(
+  val name: String,
+  val owner: String,
+  val url: String,
+  val branch: String? = "HEAD",
+  val accessTokenEnv: String? = null
+)
